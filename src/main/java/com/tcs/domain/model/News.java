b9698e236c1ee451071210352cea68d24711393b
tiagocosta <tiago.costa.silva@gmail.com>
2025-02-03 21:10:01 -0300

    Implement model and repository packages

---
+++ src/main/java/com/tcs/domain/model/News.java
@@ -0,0 +1,7 @@
+package com.tcs.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem{
+}
---
+++ src/main/java/com/tcs/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tcs.domain.repository;
+
+import com.tcs.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
